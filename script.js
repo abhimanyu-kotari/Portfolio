9aa4fb3b407e3029aac9c50b0c82a715cbb8b785
abhimanyu-kotari <abhimanyukotari4@gmail.com>
2025-06-01 20:17:17 +0530

    Add personal website

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Smooth scroll for internal links
+// Smooth scroll on anchor links (you already have this)
 document.querySelectorAll('a[href^="#"]').forEach((link) => {
   link.addEventListener("click", function (e) {
     e.preventDefault();
@@ -8,3 +8,27 @@ document.querySelectorAll('a[href^="#"]').forEach((link) => {
     }
   });
 });
+
+// Modal image popup for profile pic
+const modal = document.getElementById("photoModal");
+const modalImg = document.getElementById("modalImage");
+const captionText = document.getElementById("caption");
+const profilePic = document.querySelector(".profile-pic");
+const closeBtn = document.querySelector(".close");
+
+profilePic.onclick = function () {
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+};
+
+closeBtn.onclick = function () {
+  modal.style.display = "none";
+};
+
+// Close modal when clicking outside the image
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
